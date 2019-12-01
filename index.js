ba0a9e0b591b02dd83305bdeabb3f368c0747247
jastaffen <jack.staffen@gmail.com>
2019-12-01 11:02:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^-,'\w\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let exceptionsArr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newArr = [];
+    let sentence = string.split(" ");
+    sentence.forEach(function(word){
+      if (exceptionsArr.includes(word)) {
+        newArr.push(word);
+      } else {
+        newArr.push(Formatter.capitalize(word));
+      }
+    })
+    return Formatter.capitalize(newArr.join(" "))
+  }
 }
